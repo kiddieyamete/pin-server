4d98c777bf720506fc068fce1955c82614d83812
kiddieyamete <kiddieslot2@gmail.com>
2025-07-21 04:25:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
-const app = express(); // harus duluan
+const app = express();
 
 const PORT = process.env.PORT || 3000;
-const PIN_ADMIN = "757757"; // Ganti sesuai kebutuhan
+const PIN_ADMIN = "757757";
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +12,7 @@ app.post("/check-pin", (req, res) => {
   const { pin } = req.body;
   console.log("Diterima PIN:", pin);
   if (pin === PIN_ADMIN) {
-    res.json({ valid: true }); // GUNAKAN `valid`, bukan `success`
+    res.json({ valid: true });
   } else {
     res.json({ valid: false });
   }
